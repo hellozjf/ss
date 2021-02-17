47ede894dc326ebde7de3091d853a26d7e522aa7
Jingfeng Zhou <908686171@qq.com>
2021-02-17 14:25:51 +0800

    init

---
+++ src/main/java/com/hellozjf/project/shadowsocks/ShadowsocksApplication.java
@@ -0,0 +1,13 @@
+package com.hellozjf.project.shadowsocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadowsocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShadowsocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hellozjf/project/shadowsocks/BaseTest.java
@@ -0,0 +1,13 @@
+package com.hellozjf.project.shadowsocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BaseTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
