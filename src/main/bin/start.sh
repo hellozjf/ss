65ce86719455fe6f51170fc9f307fa1dbc84c0df
Jingfeng Zhou <908686171@qq.com>
2021-02-20 22:22:32 +0800

    使启动脚本可以在linux上面运行

--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -8,4 +8,4 @@ cd $workdir
 echo "App WorkDir is: [${workdir}]"
 
 # 启动应用，easyweb-ai应用因为要加载相似度计算的词汇向量，而词汇向量有645MB，所以我们要加大内存到2GB
-java -Xms128m -Xmx128m -jar jar/shadowsocks.jar --spring.profiles.active=test
+java -Xms128m -Xmx128m -Xlog:gc -jar jar/shadowsocks.jar --spring.profiles.active=test >log/ss.log 2>&1
--- src/main/java/com/hellozjf/project/shadowsocks/vo/UserVO.java
+++ src/main/java/com/hellozjf/project/shadowsocks/vo/UserVO.java
@@ -1,5 +1,6 @@
 package com.hellozjf.project.shadowsocks.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -16,10 +17,12 @@ public class UserVO {
 
     @ApiModelProperty(value = "创建时间")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
 
     @ApiModelProperty(value = "修改时间")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
 
     @ApiModelProperty(value = "用户名")
